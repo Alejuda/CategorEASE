4255fe4bed1d1b07e264e3fa3317f788bb7c9a19
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-12 18:29:29 -0300

    Add model/group specs

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,37 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Purchase, type: :model do
+RSpec.describe Group, type: :model do
   let!(:user) do
     User.create(name: 'Nacho', email: 'nachofino98@gmail.com', password: 'nachofino1',
                 password_confirmation: 'nachofino1')
   end
 
-  let!(:purchase) do
-    Purchase.create(name: 'Carne', amount: 1, author: user)
+  let!(:group) do
+    Group.create(name: 'Supermarket', icon: "https://icons.com", author: user)
   end
 
   context 'Validation' do
     it 'should be valid with valid attributes' do
-      expect(purchase).to be_valid
+      expect(group).to be_valid
     end
 
     it 'must have the name present' do
-      purchase.name = nil
-      expect(purchase).to_not be_valid
+      group.name = nil
+      expect(group).to_not be_valid
     end
 
     it 'must have a maximum name length of 100 characters' do
-      purchase.name = 'x' * 101
-      expect(purchase).to_not be_valid
+      group.name = 'x' * 101
+      expect(group).to_not be_valid
     end
-  end
 
-  it 'must include an amount greater than or equal to 0' do
-    purchase.amount = nil
-    expect(purchase).to_not be_valid
-
-    purchase.amount = -1
-    expect(purchase).to_not be_valid
+    it 'must include an icon' do
+      group.icon = nil
+      expect(group).to_not be_valid
+    end
   end
 
   context 'Associations' do
@@ -39,8 +36,8 @@ RSpec.describe Purchase, type: :model do
       association = described_class.reflect_on_association(:author)
       expect(association.macro).to eq(:belongs_to)
     end
-    it 'should have many groups' do
-      association = described_class.reflect_on_association(:groups)
+    it 'should have many purchases' do
+      association = described_class.reflect_on_association(:purchases)
       expect(association.macro).to eq(:has_and_belongs_to_many)
     end
   end
